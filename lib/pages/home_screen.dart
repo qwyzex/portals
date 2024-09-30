cbcfec0228d2d74e695ed01c9a10856aac73bd61
qwyzex <qwyzex@gmail.com>
2024-09-30 16:45:58 +0700

    Homescreen Appbar & BottomAppBar

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -6,28 +6,39 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:portals/components/custom_button.dart';
+import 'package:portals/components/custom_snackbar.dart';
+import 'package:portals/main.dart';
 import 'package:portals/pages/login_screen.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   Map<String, dynamic>? documentData;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // state of tabs open, home or profile
+  int openedTab = 1;
 
   Future<void> fetchDocument() async {
-    try {
-      DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance
-          .collection('users')
-          .doc('wNI1GjK2KMTO9muTIEuLtqv3K8o2')
-          .get();
+    final User? user = _auth.currentUser;
 
-      setState(() {
-        documentData = documentSnapshot.data() as Map<String, dynamic>?;
-      });
+    try {
+      if (user != null) {
+        DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(user.uid)
+            .get();
+
+        setState(() {
+          documentData = documentSnapshot.data() as Map<String, dynamic>?;
+        });
+      }
     } catch (e) {
       print('Error retrieving document: $e');
     }
@@ -40,58 +51,131 @@ class _HomeScreenState extends State<HomeScreen> {
     fetchDocument();
   }
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   SystemChrome.setSystemUIOverlayStyle(
+  //     const SystemUiOverlayStyle(
+  //       statusBarColor: Colors.transparent, // Transparent status bar
+  //       statusBarIconBrightness: Brightness.dark, // Dark icons
+  //     ),
+  //   );
+
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: const Text('Home'),
+  //     ),
+  //     body: documentData == null
+  //         ? const Center(
+  //             child:
+  //                 CircularProgressIndicator()) // Show a loader while waiting for the data
+  //         : Column(
+  //             children: [
+  //               Padding(
+  //                 padding: const EdgeInsets.all(16.0),
+  //                 child: Column(
+  //                   crossAxisAlignment: CrossAxisAlignment.start,
+  //                   children: [
+  //                     const Text(
+  //                       'User Data:',
+  //                       style: TextStyle(
+  //                           fontSize: 20, fontWeight: FontWeight.bold),
+  //                     ),
+  //                     const SizedBox(height: 10),
+  //                     // Render the document data
+  //                     Text("NIS: ${documentData?['nis']}"),
+  //                     Text("Email: ${documentData?['email']}"),
+  //                     Text("UID: ${documentData?['uid']}"),
+  //                     Text("Date Created: ${documentData?['createdAt']}"),
+  //                   ],
+  //                 ),
+  //               ),
+  //               const SizedBox(
+  //                 height: 20,
+  //               ),
+  //               SizedBox(
+  //                 width: 330,
+  //                 child: CustomButton(
+  //                   buttonColor: Colors.red,
+  //                   textColor: Colors.white,
+  //                   text: 'Log Out',
+  //                   onPressed: signOut,
+  //                 ),
+  //               ),
+  //             ],
+  //           ),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent, // Transparent status bar
-        statusBarIconBrightness: Brightness.dark, // Dark icons
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
       ),
     );
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        backgroundColor: AppColors.background,
+        title: Center(
+          child: GradientText('portalsmansa',
+              colors: const [AppColors.textColor, AppColors.textColorDim]),
+        ),
       ),
-      body: documentData == null
-          ? const Center(
-              child:
-                  CircularProgressIndicator()) // Show a loader while waiting for the data
-          : Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'User Data:',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 10),
-                      // Render the document data
-                      Text("NIS: ${documentData?['nis']}"),
-                      Text("Email: ${documentData?['email']}"),
-                      Text("UID: ${documentData?['uid']}"),
-                      Text("Date Created: ${documentData?['createdAt']}"),
-                    ],
+      body: Container(
+        decoration: const BoxDecoration(color: AppColors.background),
+        child: Center(
+          child: Text(openedTab == 1 ? 'HOME' : 'PROFILE'),
+        ),
+      ),
+      bottomSheet: BottomAppBar(
+        elevation: 2.0,
+        color: AppColors.background,
+        height: 60,
+        padding: const EdgeInsets.all(0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 120,
+              height: 60,
+              child: IconButton(
+                  icon: Icon(
+                    openedTab == 1 ? Icons.home_rounded : Icons.home_outlined,
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  width: 330,
-                  child: CustomButton(
-                    buttonColor: Colors.red,
-                    textColor: Colors.white,
-                    text: 'Log Out',
-                    onPressed: signOut,
-                  ),
-                ),
-              ],
+                  iconSize: 35,
+                  onPressed: () {
+                    setState(() {
+                      openedTab = 1;
+                    });
+                  },
+                  color: openedTab == 1
+                      ? AppColors.textColor
+                      : AppColors.textColorDim),
             ),
+            SizedBox(
+              width: 120,
+              height: 60,
+              child: IconButton(
+                icon: Icon(openedTab == 2
+                    ? Icons.account_circle_rounded
+                    : Icons.account_circle_outlined),
+                iconSize: 35,
+                onPressed: () {
+                  setState(() {
+                    openedTab = 2;
+                  });
+                },
+                color: openedTab == 2
+                    ? AppColors.textColor
+                    : AppColors.textColorDim,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -105,12 +189,11 @@ class _HomeScreenState extends State<HomeScreen> {
         MaterialPageRoute(builder: (context) => const LoginScreen()),
       );
     } on FirebaseAuthException catch (error) {
-      _showSnackBar("Failed to create account: ${error.message}");
+      customSnackbar(
+          message: "Failed to create account: ${error.message}",
+          context: context);
+    } finally {
+      customSnackbar(message: 'Successfully logged out', context: context);
     }
-  }
-
-  void _showSnackBar(String message) {
-    final snackBar = SnackBar(content: Text(message));
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
